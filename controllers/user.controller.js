3ce267b498f3a8019116717ecf2b32a7958070af
mdraselislam1944 <mdraselislam1944@gmail.com>
2023-10-19 12:22:56 +0600

    first_commit

---
+++ controllers/user.controller.js
@@ -0,0 +1,27 @@
+const User=require("../models/users.model");
+
+const getAllUsers = async(req, res) => {
+    try {
+        const result = await User.find();
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+const setUsers = async (req, res) => {
+    const {email,name}=req.body;
+    
+    if (!email || !name) {
+        return res.status(400).json({ error: "Both email and name are required." });
+    }
+    try {
+        const newUser = new User({email,name})
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+module.exports = { getAllUsers, setUsers };
---
+++ models/users.model.js
@@ -0,0 +1,18 @@
+const mongoose=require("mongoose");
+
+const userSchema=mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    createdOn:{
+        type:Date,
+        default:Date.now,
+    }
+});
+
+module.exports=mongoose.model("users",userSchema);
